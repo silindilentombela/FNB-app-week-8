cf3275a0c0b67dcfb3f708d2f1d7769405e53863
silindilentombela <amandasilindile6@gmail.com>
2025-07-14 13:12:52 +0200

    Add files via upload

---
+++ python.py
@@ -0,0 +1,20 @@
+#operators
+
+#Addition(+)
+#Subtration(-)
+#Multiplication(*)
+#Division(/)
+#Modulus(%)
+#Exponents(**)
+
+x = 10
+y = 2
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(5%2)
+
+print(x**y)
